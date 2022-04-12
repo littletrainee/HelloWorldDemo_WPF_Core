f89108b9f2960c251ff05cc32e0a98e0d9e05ed5
Logan <arechery.a1111@gmail.com>
2022-04-12 14:42:20 +0800

    Create Hello World Demo by .NET Core WPF

---
+++ HelloWorldDemo_WPF_Core/HelloWorldFolder/HelloWorld.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Controls;
+namespace HelloWorldDemo_WPF_Core.HelloWorldFolder {
+  /// <summary>
+  /// HelloWorld.xaml 的互動邏輯
+  /// </summary>
+  public partial class HelloWorld : UserControl {
+    public HelloWorld() => InitializeComponent();
+
+    private void Print(object sender, EventArgs e) =>
+      PrintLabel.Content = "Hello World!";
+  }
+}
---
+++ HelloWorldDemo_WPF_Core/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+namespace HelloWorldDemo_WPF_Core {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window {
+    public MainWindow() => InitializeComponent();
+  }
+}
